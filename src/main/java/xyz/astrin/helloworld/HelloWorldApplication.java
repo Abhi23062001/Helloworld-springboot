044fb026ad3a3fb5cd6c3bbb27c74b5947888234
Abhi23062001 <abhishekshastry90@gmail.com>
2024-03-18 21:43:56 +0530

    initialized spring project

---
+++ src/main/java/xyz/astrin/helloworld/HelloWorldApplication.java
@@ -0,0 +1,13 @@
+package xyz.astrin.helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldApplication.class, args);
+	}
+
+}
